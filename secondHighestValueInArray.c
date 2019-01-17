76ce99a110b56fc92a5244054e5d325251330f3a
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2019-01-17 10:46:30 +0100

    update of secondHighestValueInArray.c

---
+++ secondHighestValueInArray.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <limits.h>
+
+int secondHighestValueInArray(int *array, int size)
+{
+    int max = array[0];
+    int next = INT_MIN; //should be minimum value of int
+    for (int i = 0; i < size; ++i)
+    {
+        if (array[i] > max)
+        {
+            next = max;
+            max = array[i];
+        }
+        else if (array[i] > next && array[i] < max)
+        {
+            next = array[i];
+        }
+    }
+    return next;
+}
+
+int main()
+{
+    int arr[3] = {3, 1, 5};
+    printf("%d\n", secondHighestValueInArray(arr, 3));
+    return 0;
+}
