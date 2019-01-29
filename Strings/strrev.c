639c431fd7ea39e7f6090a6b4493cb0ba4ce2391
Letero <jaroslaw.kurzynski@tieto.com>
2019-01-29 12:58:02 +0100

    Update strrev.c

--- Strings/strrev.c
+++ Strings/strrev.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <mystring.h>
 
-void mystrrev(unsigned char *str)
+void mystrrev(char *str)
+{
+    unsigned int len = strlen((const char *)str);
+    char temp;
+
+    for (int i = 0, j = len - 1; i < j; ++i, --j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+void mystrrev2(unsigned char *str)
 {
     unsigned int len = strlen((const char *)str);
     unsigned char temp;
-
+    
     for (int i = 0, j = len - 1; i < j; ++i, --j)
     {
         temp = str[i];
